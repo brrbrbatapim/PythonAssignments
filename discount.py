a2630d7a07a6006c1dd0e6181956a843d45173d2
Agrata <agratashrestha36@gmail.com>
2026-05-30 14:59:52 +0545

    Discount module

---
+++ discount.py
@@ -0,0 +1,17 @@
+# discount.py
+# Own module for Week 6 Question 4
+
+TAX_RATE = 0.13
+
+
+def apply_discount(price, percent):
+    return price - (price * percent / 100)
+
+
+def apply_tax(price):
+    return price + (price * TAX_RATE)
+
+
+def final_price(price, discount_pct):
+    discounted_price = apply_discount(price, discount_pct)
+    return apply_tax(discounted_price)
